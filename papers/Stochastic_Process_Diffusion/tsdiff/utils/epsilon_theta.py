8880f7ff66e7143bfc2e6b4eb706ee3a8a029a5f
Amal Sukumaran <amal.sukumaran@cibuscell.com>
2023-07-21 16:28:43 +0200

    Add comments for network

--- papers/Stochastic_Process_Diffusion/tsdiff/utils/epsilon_theta.py
+++ papers/Stochastic_Process_Diffusion/tsdiff/utils/epsilon_theta.py
@@ -67,7 +67,7 @@ class ResidualBlock(nn.Module):
 
 
 class CondUpsampler(nn.Module):
-    def __init__(self, cond_length, target_dim):
+    def __init__(self, cond_length, target_dim): #100,8
         super().__init__()
         self.linear1 = nn.Linear(cond_length, target_dim // 2)
         self.linear2 = nn.Linear(target_dim // 2, target_dim)
@@ -83,8 +83,8 @@ class CondUpsampler(nn.Module):
 class EpsilonTheta(nn.Module):
     def __init__(
         self,
-        target_dim,
-        cond_length,
+        target_dim, ## dimensionality, for ER=8
+        cond_length, ## hidden_dim = 100 by default
         time_emb_dim=16,
         residual_layers=8,
         residual_channels=8,
@@ -123,6 +123,7 @@ class EpsilonTheta(nn.Module):
         x = F.leaky_relu(x, 0.4)
 
         diffusion_step = self.diffusion_embedding(time)
+        ## cond[1920,1,100]=>cond[1920,1,8]
         cond_up = self.cond_upsampler(cond)
         skip = []
         for layer in self.residual_layers:
