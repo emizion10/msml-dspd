f6b3a5caa7fbad6e7ffbd55bbc2b963f1aed4a83
Amal Sukumaran <amal.sukumaran@cibuscell.com>
2023-08-17 14:32:32 +0200

    Fix metrics issue

--- papers/Stochastic_Process_Diffusion/tsdiff/forecasting/metrics.py
+++ papers/Stochastic_Process_Diffusion/tsdiff/forecasting/metrics.py
@@ -7,15 +7,17 @@ def calc_crps_sample(actual, forecast):
     shuffled_forecast = rng.permutation(forecast)
     return np.absolute(forecast - actual).mean(axis=0) - 0.5 * np.absolute(forecast - shuffled_forecast).mean(axis=0)
 
-target_dim=1
+# target_dim=1
 
-test_truth = np.random.rand(5,1,30,target_dim)
-forecast = np.random.rand(5,100,30,target_dim)
+# test_truth = np.random.rand(5,1,30,target_dim)
+# forecast = np.random.rand(5,100,30,target_dim)
 
 def get_crps(forecast,test_truth):
-    target_dim = test_truth.shape[-1]    
-    forecast_reshaped = forecast.transpose(1, 0, 2, 3).reshape(100, 5, 30, target_dim)
-    test_truth_reshaped = test_truth.reshape(5,-1,target_dim)
+    num_samples = len(test_truth)
+    target_dim = test_truth.shape[-1] 
+    forecast_horizon = test_truth.shape[-2]   
+    forecast_reshaped = forecast.transpose(1, 0, 2, 3).reshape(100, num_samples, forecast_horizon, target_dim)
+    test_truth_reshaped = test_truth.reshape(num_samples,-1,target_dim)
     crps_array= calc_crps_sample(test_truth_reshaped,forecast_reshaped)
     crps = np.mean(crps_array)
     crps_mean_sample = np.mean(crps_array,axis=1)
